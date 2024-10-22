740148b251e2205e73176ca26cfb6e9f213e9800
Tuwushi <vennarciza@gmail.com>
2024-10-22 21:43:19 +0800

    modified

---
+++ countdown.js
@@ -0,0 +1,22 @@
+const countdownDate = new Date("Nov 5, 2024 00:00:00").getTime();
+const button = document.getElementById("countdown-btn");
+
+const countdownFunction = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        button.disabled = false;
+    }
+}, 1000);
